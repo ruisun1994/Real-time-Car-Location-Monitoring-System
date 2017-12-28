e3dfc3859ba6f323f9071736ea9a1ad012a91980
Yangfei Li <lyfelmer@outlook.com>
2017-12-28 13:26:00 -0500

    config

---
+++ src/main/java/com/spring/uber/HelloWorldInitializer.java
@@ -0,0 +1,18 @@
+package com.spring.uber;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class HelloWorldInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
+}
