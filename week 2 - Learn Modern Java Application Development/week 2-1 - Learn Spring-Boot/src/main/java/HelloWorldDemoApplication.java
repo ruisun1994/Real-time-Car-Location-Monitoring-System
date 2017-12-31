367c3a6bb0787ff7fb8086a333b5700fdb665020
Yangfei Li <lyfelmer@outlook.com>
2017-12-30 23:44:16 -0500

    week 2-1 Learn Spring-Boot

---
+++ week 2 - Learn Modern Java Application Development/week 2-1 - Learn Spring-Boot/src/main/java/HelloWorldDemoApplication.java
@@ -0,0 +1,13 @@
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class HelloWorldDemoApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(HelloWorldDemoApplication.class, args);
+
+    }
+}
